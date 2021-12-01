d28c1cbafb43a7f7d33d8c25ca242b1716301217
Yu Jin Choi <70509403+yujch716@users.noreply.github.com>
2021-12-01 10:23:09 +0900

    211130_find

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,18 @@ Route::get('/join', function (){
     return view('join');
 });
 
-Route::post('/join_ok',[App\Http\Controllers\UserController::class,'join_ok']);
+Route::get('/find_email', function (){
+    return view('find_email');
+});
 
-Route::post('/email_multiple',[App\Http\Controllers\UserController::class,'email_multiple']);
+Route::post('/join_ok',[App\Http\Controllers\UserController::class,'join_ok']); //회원가입
+
+Route::post('/email_multiple',[App\Http\Controllers\UserController::class,'email_multiple']); //이메일 중복확인
 
 Route::get('ajaxRequest', [UserController::class, 'ajaxRequest']);
 Route::post('ajaxRequest', [UserController::class, 'ajaxRequestPost']);
 
-Route::post('/login_ok',[App\Http\Controllers\UserController::class,'login_ok']);
-Route::get('/logout',[App\Http\Controllers\UserController::class, 'logout']);
+Route::post('/login_ok',[App\Http\Controllers\UserController::class,'login_ok']); //로그인
+Route::get('/logout',[App\Http\Controllers\UserController::class, 'logout']); //로그아웃
+
+Route::get('/find_email',[App\Http\Controllers\UserController::class,'find_email']); //이메일 찾기
