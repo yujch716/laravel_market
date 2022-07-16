4dee352375ebc4fe15a3fe9ba922f090c7eed6aa
yujch716 <yujch716@gmail.com>
2022-07-16 17:03:57 +0900

    헤더 수정함

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,11 +17,11 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="/"><b>Shop</b></a>
+        <a class="navbar-brand" href="#!">Start Bootstrap</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Home</a></li>
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
@@ -46,25 +46,14 @@
             @else
                 <a href="/login" class="btn btn-outline-dark" style="margin: 5px">Login</a>
                 <a href="/join" class="btn btn-outline-dark" style="margin: 5px">Join</a>
-                <form class="d-flex">
-                    <button class="btn btn-outline-dark" style="margin: 5px" type="submit">
-                        <i class="bi-cart-fill me-1"></i>
-                        Cart
-                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                    </button>
-                </form>
             @endif
+
         </div>
     </div>
 </nav>
-<!-- Section-->
-<section class="bg-dark py-5">
-    <div class="container px-4 px-lg-5 my-5">
-        <div class="text-center text-white">
-            @yield('section')
-        </div>
-    </div>
-</section>
+
+@yield('section')
+
 <!-- Footer-->
 <footer class="py-5 bg-dark">
     <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
